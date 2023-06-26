ecb09701a199420fcb8aa0f3831175ac6c62d397
tnkyc <hakuandmei@yahoo.co.jp>
2023-06-26 14:05:12 +0900

    イベント発火基礎編

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists)
+const pullDownButton = document.getElementById("lists");
+
+pullDownButton.addEventListener('mouseover',function(event) {
+    console.log('乗る');
+});
+
+pullDownButton.addEventListener('mouseout',function(event) {
+    console.log('離れる');
+});
+
+pullDownButton.addEventListener('click',function(event) {
+    console.log('押す');
+});
