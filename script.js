5f66b15c376bde7a4f27b03ad766f4ea935b84ca
tnkyc <hakuandmei@yahoo.co.jp>
2023-06-27 16:19:50 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const pullDownButton = document.getElementById("lists");
 
 pullDownButton.addEventListener('mouseover',function(event) {
-    console.log('乗る');
+   console.log('乗ると青色');
+   pullDownButton.setAttribute("style","background-color:blue;")
 });
 
 pullDownButton.addEventListener('mouseout',function(event) {
-    console.log('離れる');
+    console.log('離れると赤色');
+    pullDownButton.removeAttribute("style","background-color:red;")
 });
 
 pullDownButton.addEventListener('click',function(event) {
-    console.log('押す');
+    console.log('押すと緑色');
+    pullDownButton.setAttribute("style","background-color:green;")
 });
