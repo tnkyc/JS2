7098d3ab3540e9ccc9e17a69b9349e0c2b804c06
tnkyc <hakuandmei@yahoo.co.jp>
2023-06-27 20:37:55 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const pullDownButton = document.getElementById("lists");
 
 pullDownButton.addEventListener('mouseover',function(event) {
    console.log('乗ると青色');
-   pullDownButton.setAttribute("style","background-color:blue;")
+   this.setAttribute("style","background-color:blue;")
 });
 
 pullDownButton.addEventListener('mouseout',function(event) {
     console.log('離れると赤色');
-    pullDownButton.removeAttribute("style","background-color:red;")
+    this.removeAttribute("style","background-color:red;")
 });
 
-pullDownButton.addEventListener('click',function(event) {
-    console.log('押すと緑色');
-    pullDownButton.setAttribute("style","background-color:green;")
-});
+
+const pullDownParents = document.getElementById("pull-down")
+
+this.addEventListener('click', function(event) {
+    if (pullDownParents.getAttribute("style") == "display:block;"){
+        pullDownParents.removeAttribute("style", "display:block;")
+        console.log("非表示")
+    } else {
+        pullDownParents.setAttribute("style", "display:block;")
+        console.log("表示")
+    }
+  })
